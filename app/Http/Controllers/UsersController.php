1525745694d4f414264ad64dc516ac7e22714852
vagrant <vagrant@homestead>
2023-04-21 02:20:08 +0000

    增加 CSRF 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -17,4 +17,14 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
+    // required 表示不能为空  unique:users 表示唯一的 confirmed 表示匹配两次输入的密码是否相同 email 表示格式验证
+    public function store(Request $request){
+        $this->validate($request,[
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|unique:users|email|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
+    }
+
 }
